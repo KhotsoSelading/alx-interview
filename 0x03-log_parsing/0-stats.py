7cbff185827c982391997222a6af07b9c3c78127
KhotsoSelading <k.selading007@gmail.com>
2024-01-23 11:13:30 +0200

    Task Complete - Khotso

--- 0x03-log_parsing/0-stats.py
+++ 0x03-log_parsing/0-stats.py
@@ -4,40 +4,37 @@ Topic: Log Parsing
 Author: Khotso Selading
 Date: 22-01-2024
 """
-
 import sys
 
 if __name__ == '__main__':
 
-    filesize = 0
-    count = 0
-    codes = {"200", "301", "400", "401", "403", "404", "405", "500"}
+    filesize, count = 0, 0
+    codes = ["200", "301", "400", "401", "403", "404", "405", "500"]
     stats = {k: 0 for k in codes}
 
     def print_stats(stats: dict, file_size: int) -> None:
-        print("Total File size: {:d}".format(file_size))
+        print("File size: {:d}".format(filesize))
         for k, v in sorted(stats.items()):
             if v:
                 print("{}: {}".format(k, v))
 
     try:
-        for line_spliter in sys.stdin:
+        for line in sys.stdin:
             count += 1
-            data = line_spliter.split()
+            data = line.split()
             try:
                 status_code = data[-2]
-                if status_code in codes:
+                if status_code in stats:
                     stats[status_code] += 1
-            except IndexError:
+            except BaseException:
                 pass
             try:
                 filesize += int(data[-1])
-            except (ValueError, IndexError):
+            except BaseException:
                 pass
             if count % 10 == 0:
                 print_stats(stats, filesize)
         print_stats(stats, filesize)
-
     except KeyboardInterrupt:
         print_stats(stats, filesize)
         raise
