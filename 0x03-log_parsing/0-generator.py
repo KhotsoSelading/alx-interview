5230b0bd3b0a588ecb56903e7b617ac57816781d
KhotsoSelading <k.selading007@gmail.com>
2024-01-23 11:02:24 +0200

    Task Complete - Khotso

--- 0x03-log_parsing/0-generator.py
+++ 0x03-log_parsing/0-generator.py
@@ -1,10 +1,4 @@
 #!/usr/bin/python3
-"""
-Topic: Log Parsing
-Author: Khotso Selading
-Date: 22-01-2024
-"""
-
 import random
 import sys
 from time import sleep
@@ -12,16 +6,10 @@ import datetime
 
 for i in range(10000):
     sleep(random.random())
-    sys.stdout.write(
-        "{:d}.{:d}.{:d}.{:d} - [{}] \"GET /projects/260 HTTP/1.1\" {} {}\n"
-        .format(
-            random.randint(1, 255),
-            random.randint(1, 255),
-            random.randint(1, 255),
-            random.randint(1, 255),
-            datetime.datetime.now(),
-            random.choice([200, 301, 400, 401, 403, 404, 405, 500]),
-            random.randint(1, 1024)
-        )
-    )
+    sys.stdout.write("{:d}.{:d}.{:d}.{:d} - [{}] \"GET /projects/260 HTTP/1.1\" {} {}\n".format(
+        random.randint(1, 255), random.randint(1, 255), random.randint(1, 255), random.randint(1, 255),
+        datetime.datetime.now(),
+        random.choice([200, 301, 400, 401, 403, 404, 405, 500]),
+        random.randint(1, 1024)
+    ))
     sys.stdout.flush()
