0940102de607f097c8ebd768043492a3486714f7
KhotsoSelading <k.selading007@gmail.com>
2024-02-20 13:12:36 +0200

    Task Complete

--- 0x07-rotate_2d_matrix/0-rotate_2d_matrix.py
+++ 0x07-rotate_2d_matrix/0-rotate_2d_matrix.py
@@ -16,9 +16,4 @@ def rotate_2d_matrix(matrix):
     Returns:
     None
     """
-    n = len(matrix)
-    for i in range(n):
-        for j in range(i, n):
-            matrix[i][j], matrix[j][i] = matrix[j][i], matrix[i][j]
-    for i in range(n):
-        matrix[i].reverse()
+    matrix[:] = [[row[i] for row in matrix[::-1]] for i in range(len(matrix))]
