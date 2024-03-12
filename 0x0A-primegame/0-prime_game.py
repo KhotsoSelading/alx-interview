e5ef8e86566d5e87d61fc5fdca8682ba5c879cd1
KhotsoSelading <k.selading007@gmail.com>
2024-03-12 15:29:04 +0200

    Task complete - Khotso

--- 0x0A-primegame/0-prime_game.py
+++ 0x0A-primegame/0-prime_game.py
@@ -7,22 +7,25 @@ Date: 10-03-2024
 
 
 def isWinner(x, nums):
-    """Deteremins winner of the prime number game"""
+    """Determines winner of the prime number game"""
     if x < 1 or not nums:
         return None
+
+    max_n = max(nums)
+    primes = [True] * (max_n + 1)
+    primes[0], primes[1] = False, False
+
+    for i in range(2, int(max_n**0.5) + 1):
+        if primes[i]:
+            for j in range(i*i, max_n + 1, i):
+                primes[j] = False
+
     marias_wins, bens_wins = 0, 0
-    n = max(nums)
-    primes = [True for _ in range(1, n + 1, 1)]
-    primes[0] = False
-    for i, is_prime in enumerate(primes, 1):
-        if i == 1 or not is_prime:
-            continue
-        for j in range(i + i, n + 1, i):
-            primes[j - 1] = False
-    for _, n in zip(range(x), nums):
-        primes_count = len(list(filter(lambda x: x, primes[0: n])))
+    for n in nums:
+        primes_count = sum(primes[:n+1])
         bens_wins += primes_count % 2 == 0
         marias_wins += primes_count % 2 == 1
+
     if marias_wins == bens_wins:
         return None
     return 'Maria' if marias_wins > bens_wins else 'Ben'
