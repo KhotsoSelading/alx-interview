3590618b7461c79afe9b55319b8bb2093e5afc73
KhotsoSelading <k.selading007@gmail.com>
2024-03-12 15:02:20 +0200

    Task Complete - Khotso

--- 0x0A-primegame/0-prime_game.py
+++ 0x0A-primegame/0-prime_game.py
@@ -6,45 +6,75 @@ Date: 10-03-2024
 """
 
 
-def generate_primes(limit):
+def findMultiples(num, targets):
     """
-    Finds prime number
+    Finds multiples of a given number within a list
     """
-    primes = [True] * (limit + 1)
-    primes[0] = primes[1] = False
-    for i in range(2, int(limit ** 0.5) + 1):
-        if primes[i]:
-            for j in range(i * i, limit + 1, i):
-                primes[j] = False
-    return [i for i in range(limit + 1) if primes[i]]
+    for i in targets:
+        if i % num == 0:
+            targets.remove(i)
+    return targets
 
 
-def determine_winner(n):
+def isPrime(i):
     """
-    Deteremines winner
+    Check if a number is prime.
     """
-    primes = generate_primes(n)
-    if len(primes) % 2 == 0:
-        return "Ben"
-    else:
-        return "Maria"
+    if i == 1:
+        return False
+    for j in range(2, i):
+        if i % j == 0:
+            return False
+    return True
+
+
+def findPrimes(n):
+    """
+    Dispatch a given set into prime numbers and non-prime numbers.
+    """
+    counter = 0
+    target = list(n)
+    for i in range(1, len(target) + 1):
+        if isPrime(i):
+            counter += 1
+            target.remove(i)
+            target = findMultiples(i, target)
+        else:
+            pass
+    return counter
 
 
 def isWinner(x, nums):
     """
-    Gets winner
-    """
-    maria_wins = ben_wins = 0
-    for n in nums:
-        winner = determine_winner(n)
-        if winner == "Maria":
-            maria_wins += 1
-        elif winner == "Ben":
-            ben_wins += 1
+    Maria and Ben are playing a game.Given a set of consecutive integers
+    starting from 1 up to and including n, they take turns choosing a
+    prime number from the set and removing that number and its
+    multiples from the set.
+    The player that cannot make a move loses the game.
 
-    if maria_wins > ben_wins:
-        return "Maria"
-    elif maria_wins < ben_wins:
-        return "Ben"
+    They play x rounds of the game, where n may be different for each round.
+    Assuming Maria always goes first and both players play optimally,
+    determine who the winner of each game is.
+    """
+    players = {'Maria': 0, 'Ben': 0}
+    cluster = set()
+    for elem in range(x):
+        nums.sort()
+        num = nums[elem]
+        for i in range(1, num + 1):
+            cluster.add(i)
+            if i == num + 1:
+                break
+        temp = findPrimes(cluster)
+
+        if temp % 2 == 0:
+            players['Ben'] += 1
+        elif temp % 2 != 0:
+            players['Maria'] += 1
+
+    if players['Maria'] > players['Ben']:
+        return 'Maria'
+    elif players['Maria'] < players['Ben']:
+        return 'Ben'
     else:
         return None
